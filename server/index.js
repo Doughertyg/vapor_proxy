6870f85ce06d56177378522c1627cae9e28c327d
Grahams Laptop <doughertyg37@gmail.com>
2019-05-31 14:47:59 -0700

    added webpack externals for styled components, added styled components and react cdn links to html, and replaced import styled components with window.styled

--- server/index.js
+++ server/index.js
@@ -1,9 +1,184 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const BodyParser = require('body-parser');
+const request = require('request');
 
-app.use(BodyParser.json());
 app.use(express.static('public'));
+/*port 3001*/ ///Jimmy's api's
+app.get('/game', (req, res) => {
+  request("http://localhost:3002/game", (err, response, body) => {
+    if(err) {
+      res.status(400);
+      res.send(err);
+    } else {
+      res.status(200);
+      res.send(body);
+    }
+  });
+});
 
-app.listen(port, () => console.log(`listening on port ${port}!`));
+/*port 3002*/ ///xin's api's
+app.get('/games/:uid', (req, res) => {
+  request("http://localhost:3002/games/"+ req.params.uid, (err, response, body) => {
+    if(err) {
+      res.status(400);
+      res.send(err);
+    } else {
+      res.status(200);
+      res.send(body);
+    }
+  });
+});
+
+app.get('/screenshots', (req, res) => {
+  request("http://localhost:3002/screenshots", (err, response, body) => {
+    if(err) {
+      res.status(400);
+      res.send(err);
+    } else {
+      res.status(200);
+      res.send(body);
+    }
+  });
+});
+
+app.get('/videos', (req, res) => {
+  request("http://localhost:3002/videos", (err, response, body) => {
+    if(err) {
+      res.status(400);
+      res.send(err);
+    } else {
+      res.status(200);
+      res.send(body);
+    }
+  });
+});
+
+// ERIC'S APIS
+app.get('/reviews', (req, res) => {
+  console.log('TRYING TO GET REVIEWS');
+  let options = {
+    url: 'http://localhost:3005/reviews',
+    json: true,
+    body: req.query
+  };
+
+  request(options, function(error, response, body) {
+    if (error) { console.error('Could not get reviews data', error); }
+    console.log('Type of reviews object is ' + typeof body);
+    res.send(body);
+  });
+});
+
+app.get('/recent', (req, res) => {
+  console.log('TRYING TO GET RECENT');
+  let options = {
+    url: 'http://localhost:3005/recent',
+    json: true,
+    body: req.query
+  };
+  
+  request(options, function(error, response, body) {
+    if (error) { console.error('Could not get reviews data', error); }
+    console.log('Type of reviews object is ' + typeof body);
+    res.send(body);
+  });
+});
+
+app.post('/review/vote', (req, res) => {
+  const data = {
+    post_id: req.body.post_id,
+    helpfulness: req.body.helpfulness
+  };
+
+  const options = {
+    url: 'http://localhost:3005/review/vote',
+    method: 'POST',
+    json: true,
+    form: data
+  };
+
+  request(options , function(error, response, body) {
+    if (error) { console.error('Could not get reviews data', error); }
+    console.log('Type of reviews object is ' + typeof body);
+    res.send(body);
+  });
+});
+
+app.get('/reviews/filters', (req, res) => {
+  request('http://localhost:3005/reviews/filters', function(error, response, body) {
+    if (error) { console.error('Could not get languages', error); }
+    res.send(JSON.parse(body));
+  });
+});
+
+app.get('/reviews/comments', (req, res) => {
+  console.log('TRYING TO GET COMMENTS');
+  let options = {
+    url: 'http://localhost:3005/reviews/comments',
+    json: true,
+    body: req.query
+  };
+
+  request(options, function(error, response, body) {
+    if (error) { console.error('Could not get reviews data', error); }
+    console.log('Type of reviews object is ' + typeof body);
+    res.send(body);
+  });
+});
+
+app.post('/reviews/comment', (req, res) => {
+  const options = {
+    url: 'http://localhost:3005/reviews/comment',
+    method: 'POST',
+    json: true,
+    form: req.body.data
+  };
+
+  request(options , function(error, response, body) {
+    if (error) { console.error('Could not get reviews data', error); }
+    console.log('Type of reviews object is ' + typeof body);
+    res.send(body);
+  });
+});
+
+app.get('/graphOverall', (req, res) => {
+  request('http://localhost:3005/graphOverall', function(error, response, body) {
+    if (error) { console.error('Could not get graph data', error); }
+    res.send(JSON.parse(body));
+  });
+})
+
+app.get('/graphRecent', (req, res) => {
+  console.log('ROUTE TO ERIC');
+  request('http://localhost:3005/graphRecent', function(error, response, body) {
+    if (error) { console.error('Could not get graph data', error); }
+    res.send(JSON.parse(body));
+  });
+})
+
+//////////Graham's api's port 3003
+app.get('/Updates', (req, res) => {
+
+  request('http://localhost:3003/Updates', (err, res, body) => {
+    if(err) {
+      res.status(404).send(err);
+    }
+
+    res.status(200).send(body);
+  })
+})
+
+app.get('/Games/:id', (req, res) => {
+
+  request(`http://localhost:3003/Games/${req.params.id}`, (err, res, body) => {
+    if(err) {
+      res.status(404).send(err);
+    }
+
+    res.status(200).send(body);
+  })
+})
+
+app.listen(3000, (req, res) => {
+  console.log('listening to port 3000...');
+})
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+	entry: `${SRC_DIR}/index.jsx`,
+	output: {
+		filename: 'bundle.js',
+		path: DIST_DIR
+	},
+	externals: {
+    	'styled-components': {
+      		commonjs: 'styled-components',
+      		commonjs2: 'styled-components',
+      		amd: 'styled-components'
+    	}
+  	},
+	module: {
+		rules: [
+		{
+			test: /\.jsx?/,
+			include: SRC_DIR,
+			use: {
+				loader: 'babel-loader',
+				query: {
+					presets: ['@babel/preset-react', '@babel/env']
+				}				
+			}
+		}]
+	}
+
+}
